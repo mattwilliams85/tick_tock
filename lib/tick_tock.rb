218367cfb24fc021f6f754535fe3c5a0d73502cd
Matthew & Jeremy Whitaker <student@epicodus.com>
2014-07-25 11:30:53 -0700

    adds working degree difference function for hour and minutes

--- lib/tick_tock.rb
+++ lib/tick_tock.rb
@@ -1,3 +1,21 @@
 def tick_tock(hour,minutes)
-  minDegrees = minutes * 6
+  hour_degrees = hour * 30
+  min_degrees = minutes * 6
+
+  if hour == 12
+    hour_degrees = 0
+  end
+
+  hour_degrees += 0.5*minutes
+
+  result = (hour_degrees.to_i - min_degrees.to_i).abs
+  if result > 180
+     result = 360 - result
+  end
+
+  p "The difference in degrees between the hour and minute hand is " + result.to_s + " degrees."
+  result
 end
+
+
+tick_tock(8,20)
--- spec/tick_tock_spec.rb
+++ spec/tick_tock_spec.rb
@@ -2,14 +2,26 @@ require('rspec')
 require('tick_tock')
 
 describe("tick_tock") do
-  it("converts inputted minutes into degrees") do
-    tick_tock(3,45)
-    tick_tock.minDegrees.should(eq(270))
+  it("converts inputted minutes and hour into degrees") do
+    tick_tock(3,00).should(eq(90))
   end
+
+  it("assigns the correct degree value for 12 o'clock") do
+    tick_tock(12,00).should(eq(0))
+  end
+
+  it("properly calculates when hour hand is before 6 o'clock") do
+    tick_tock(3,30).should(eq(75))
+  end
+
+  it("properly calculates when hour hand is after 6 o'clock") do
+    tick_tock(8,20).should(eq(130))
+  end
+end
 
 # describe("tick_tock") do
 #   it("converts inputted hour into degrees") do
 #     tick_tock().should(eq("fizz"))
 #   end
-end
+
 
